5f67a5d21cb4f3607b866e01077f48af5ad75f2d
xenongee <rublgleb@gmail.com>
2024-03-06 12:57:00 +0300

    release

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,34 @@
 /**
- * CDS Vyatka monotoring screen for KAT 
+ * CDS Vyatka Monotoring screen for KAT
  * Public transportation monitoring screen from CDS on two route directions for Kirov Aviation Technical School
  *
  * Author: @xenongee
  * Created: 01.03.2024
  * Last update: 06.03.2024
  */
-
 const doc = document;
 const url = "https://some.url/aviatech/index.php";
 const fnGosNum = true;
 const fnSortByTime = false;
-const tableID = "#cds-table";
-const tableColumn = {
-    train: { prefix: "#column-train", kod: "261", header: "В сторону Ж/Д" },
-    folk: { prefix: "#column-folk", kod: "262", header: "В сторону ОДНТ" },
+const tbl = "#cds-table";
+const tblCol = "#column";
+const tblRow = "#row";
+const tblColTemplate = "#cds-table-col-template";
+const tblRowTemplate = "#cds-table-row-template";
+const tableColumns = {
+	train: { prefix: "#column-train", kod: "261", header: "В сторону Ж/Д" },
+	folk: { prefix: "#column-folk", kod: "262", header: "В сторону ОДНТ" },
 };
-const tableRow = {
-    marsh: { prefix: "#content-bus #bus", updateAnim: false },
-    gosnum: { prefix: "#content-bus #gosnum", updateAnim: false },
-    minutes: { prefix: "#content-time", updateAnim: true },
+const tableRows = {
+	marsh: { prefix: "#content-bus #bus", updateAnim: false },
+	gosnum: { prefix: "#content-bus #gosnum", updateAnim: false },
+	minutes: { prefix: "#content-time", updateAnim: true },
 };
 const dateOptions = { dateStyle: "short", timeStyle: "medium" };
-const processTick = 25000;
+const processTick = 10000;
 const processTickWhenError = 300000;
-let intervalProcess, intervalClock, currentDate, pastDate, temp;
+// biome-ignore lint/style/useSingleVarDeclarator: <explanation>
+let  intervalProcess, intervalClock, currentDate, pastDate, temp;
 
 async function getData(url) {
     const response = await fetch(url).catch((err) => {
@@ -43,16 +47,16 @@ async function getData(url) {
 }
 
 function prepareData(data) {
-    const dataVirtual = structuredClone(tableColumn);
-    for (const col of Object.keys(tableColumn)) {
+    const dataVirtual = structuredClone(tableColumns);
+    for (const col of Object.keys(tableColumns)) {
         dataVirtual[col].prefix = undefined;
         dataVirtual[col].kod = undefined;
         dataVirtual[col] = [];
     }
     data.sort(fnSortByTime ? (a, b) => a.minutes - b.minutes : (a, b) => a.marsh - b.marsh);
     const dataPrepared = data.reduce((acc, el) => {
-        const key = Object.keys(tableColumn).find((col) => el.kod === tableColumn[col].kod);
-        if (!(key in tableColumn)) {
+        const key = Object.keys(tableColumns).find((col) => el.kod === tableColumns[col].kod);
+        if (!(key in tableColumns)) {
             showMsg(`Код маршрута не существует: ${el.kod}`, true);
             throw new Error(`Code (${el.kod}) in not exist`);
         }
@@ -64,49 +68,59 @@ function prepareData(data) {
         } else {
             minutes = `${el.minutes} мин.`;
         }
-        acc[key].push({ ...el, minutes });
+        acc[key].push({...el, minutes});
         return acc;
     }, dataVirtual);
     return dataPrepared;
 }
 
-function drawRowsInColumns(data) {
-    const tableRowTemplate = doc.querySelector(`${tableID} > #cds-table-row-template`).content;
-    const tableColumnsVirtual = {};
-    for (const col of Object.keys(tableColumn)) {
-        doc.querySelector(`${tableID} > #column > .table-header${tableColumn[col].prefix}`).innerHTML = tableColumn[col].header;
-        tableColumnsVirtual[col] = doc.querySelector(`${tableID} > #column > .table-column${tableColumn[col].prefix}`);
-        tableColumnsVirtual[col].innerHTML = ""; // pre-clear
-    }
-    if (!fnGosNum) tableRowTemplate.querySelector("small#gosnum").remove();
-    for (const col of Object.keys(tableColumn)) {
+function drawRowsInColumns(data, createColumn) {
+    const tableWrapper = doc.querySelector(tbl);
+    const tableColumnTemplate = doc.querySelector(tblColTemplate).content;
+    const tableRowTemplate = doc.querySelector(tblRowTemplate).content;
+    const tableColumnsWrapper = {};
+    if (!fnGosNum) tableRowTemplate.querySelector("small#gosnum")?.remove();
+    for (const col of Object.keys(tableColumns)) {
+        if (createColumn) {
+            const tableColumnClone = tableColumnTemplate.cloneNode(true);
+            tableColumnClone.querySelector(`${tblCol}-header`).id = `${tblCol.slice(1)}-${col}-header`
+            tableColumnClone.querySelector(`${tblCol}-content`).id = `${tblCol.slice(1)}-${col}`
+            tableWrapper.appendChild(tableColumnClone);
+        }
+        doc.querySelector(`${tbl} > ${tblCol} > ${tableColumns[col].prefix}-header`).innerHTML = tableColumns[col].header;
+        tableColumnsWrapper[col] = doc.querySelector(`${tbl} > ${tblCol} > ${tableColumns[col].prefix}`);
+        tableColumnsWrapper[col].innerHTML = ""; // pre-clear
         if (data[col].length === 0) {
-            tableColumnsVirtual[col].innerHTML = "<h1>Транспорт отсутствует</h1>";
+            tableColumnsWrapper[col].innerHTML = "<h1>Транспорт отсутствует</h1>";
         } else {
             for (const i of data[col]) {
-                tableColumnsVirtual[col].appendChild(tableRowTemplate.cloneNode(true));
+                tableColumnsWrapper[col].appendChild(tableRowTemplate.cloneNode(true));
             }
         }
     }
 }
 
 function updateTable(data, offUpdateAnim) {
-    const tableVirtual = structuredClone(tableColumn);
-    for (const col of Object.keys(tableColumn)) {
+    const tableVirtual = structuredClone(tableColumns);
+    for (const col of Object.keys(tableColumns)) {
         tableVirtual[col].prefix = undefined;
-        for (const row of Object.keys(tableRow)) {
-            tableVirtual[col][row] = doc.querySelectorAll(`${tableID} > #column > .table-column${tableColumn[col].prefix} > #row ${tableRow[row].prefix}`);
+        for (const row of Object.keys(tableRows)) {
+            tableVirtual[col][row] = doc.querySelectorAll(`${tbl} > ${tblCol} > ${tableColumns[col].prefix} > ${tblRow} ${tableRows[row].prefix}`);
         }
-    }
-    for (const col of Object.keys(tableColumn)) {
-        for (const row of Object.keys(tableRow)) {
-            // check data sizes with table size
+        test = tableVirtual
+        console.log(test);
+        for (const row of Object.keys(tableRows)) {
+            if (!fnGosNum && row === "gosnum") {
+                tableVirtual[col].gosnum = undefined;
+                continue;
+            }
+            // checking data sizes with table size, if sizes are not equal, table will be cleared
             if (tableVirtual[col][row].length !== data[col].length) drawRowsInColumns(data);
             // append data in table
-            data[col].forEach((el, i) => {
+            for(const [i, el] of data[col].entries()) {
                 tableVirtual[col][row][i].innerHTML = el[row];
                 if (offUpdateAnim && temp) showUpdatesAnim(i, col, data, temp, tableVirtual);
-            });
+            }
         }
     }
 }
@@ -122,22 +136,6 @@ function checkFreshData(data) {
     if (JSON.stringify(temp) !== JSON.stringify(data)) pastDate = `Данные от ${currentDate}.`;
     temp = data;
     showMsg(`Текущее время ${currentDate}. ${pastDate ?? ""} (+)`);
-}
-
-function skeletonFlowAnim(status) {
-    for (const el of doc.querySelectorAll(`${tableID} > #column #row`)) {
-        el.classList.toggle("flow", Boolean(status));
-    }
-}
-
-function showUpdatesAnim(iter, col, data, table, tableVirtual) {
-    for (const rowEl of Object.entries(tableRow)) {
-        const row = rowEl[0];
-        const rowUpdateAnim = rowEl[1].updateAnim;
-        const tableVirtualValue = tableVirtual[col][row][iter];
-        if (rowUpdateAnim && (!table[col][iter] || table[col][iter][row] !== data[col][iter][row]))
-            setTimeout(() => tableVirtualValue.classList.remove("updated"), 3000, tableVirtualValue.classList.add("updated"));
-    }
 }
 
 const showMsg = (msg, err) => {
@@ -158,6 +156,47 @@ const showMsg = (msg, err) => {
     msgLine.classList.remove("hide");
 };
 
+function showUpdatesAnim(iter, col, data, table, tableVirtual) {
+    for (const rowEl of Object.entries(tableRows)) {
+        const row = rowEl[0];
+        const rowUpdateAnim = rowEl[1].updateAnim;
+        const tableVirtualValue = tableVirtual[col][row][iter];
+        if (rowUpdateAnim && (!table[col][iter] || table[col][iter][row] !== data[col][iter][row]))
+            setTimeout(() => tableVirtualValue.classList.remove("updated"), 3000, tableVirtualValue.classList.add("updated"));
+    }
+}
+
+function skeletonFlowAnim(status) {
+    for (const el of doc.querySelectorAll(`${tbl} > #column #row`)) {
+        el.classList.toggle("flow", Boolean(status));
+    }
+}
+
+function drawSkeletonTable() {
+    const tempJson = [
+		{ marsh: "temp", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "33а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "10а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "53а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "70а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "90а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "61а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "3т", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
+		{ marsh: "54а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "33а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "39а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "53а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "70а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "90а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "61а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
+		{ marsh: "3т", minutes: "00", gosnum: "А 000 АА 43", kod: "262" }
+	];
+    const tempdata = prepareData(tempJson);
+    drawRowsInColumns(tempdata, true);
+    skeletonFlowAnim(false);
+    updateTable(tempdata, false);
+}
+
 async function update() {
     const data = prepareData(await getData(url));
     drawRowsInColumns(data);
@@ -167,30 +206,10 @@ async function update() {
 }
 
 function start() {
-    const tempJson = [
-        { marsh: "temp", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "33а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "10а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "53а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "70а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "90а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "61а", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "3т", minutes: "00", gosnum: "А 000 АА 43", kod: "261" },
-        { marsh: "54а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "33а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "39а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "53а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "70а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "90а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "61а", minutes: "00", gosnum: "А 000 АА 43", kod: "262" },
-        { marsh: "3т", minutes: "00", gosnum: "А 000 АА 43", kod: "262" }
-    ];
-    const tempdata = prepareData(tempJson);
-    drawRowsInColumns(tempdata);
-    updateTable(tempdata, false);
+    drawSkeletonTable();
     setTimeout(() => {
         update(url);
-    }, 1500);
+    }, 3500);
     intervalProcess = setInterval(() => {
         update(url);
     }, processTick);
